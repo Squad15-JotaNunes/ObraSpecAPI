612900df721415e5eec468a43642bcff2dac4e6f
Kaio Vinícios <kvsgpro@outlook.com>
2025-08-28 20:56:36 -0300

    feat(django): initialize project structure, settings and create initial apps

---
+++ apps/accounts/admin.py
@@ -0,0 +1,18 @@
+# admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserPermissions
+
+class UserPermissionsInline(admin.StackedInline):
+    model = UserPermissions
+    can_delete = False
+    verbose_name_plural = "Permissions"
+
+# Extendendo o admin do User original
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserPermissionsInline,)
+
+# Desregistrar o User original e registrar o novo
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
---
+++ apps/accounts/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserType(models.TextChoices):
+    # ADMIN = "admin", "Admin"
+    SPECIFIER = "specifier", "Specifier"
+    REVIEWER = "reviewer", "Reviewer"
+
+class UserPermissions(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(
+        max_length=20, choices=UserType.choices, default=UserType.SPECIFIER
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - ({self.user_type})"
