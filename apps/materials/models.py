1d89f21ce663d2ee29ea38418ca2a8feecee7ea9
Kaio Vinícios <kvsgpro@outlook.com>
2025-09-04 21:51:22 -0300

    feat(apps): create materials app

---
+++ apps/materials/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class MaterialType(models.Model):
+    name = models.CharField(_("Material Type Name"), max_length=50, unique=True)
+
+
+# SetBrandMaterialType
+class Material(models.Model):
+    description = models.TextField(verbose_name=_("description"), blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="materials")
+    material_type = models.ForeignKey(MaterialType, on_delete=models.CASCADE, related_name="materials")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["brand", "material_type"], name="unique_brand_material_type")
+        ]
+
+    def __str__(self):
+        return f"{self.material_type} - {self.brand}"
