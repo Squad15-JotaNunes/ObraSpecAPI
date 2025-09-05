cf29f45959225aa1a80def2bd1fa4ff3445b7d78
William Santana <williamandrade1058@gmail.com>
2025-09-05 19:03:53 -0300

    feat(models): add many-to-many relation between Construction and Observation

--- apps/constructions/models.py
+++ apps/constructions/models.py
@@ -1,21 +1,56 @@
 from django.db import models
 from apps.observations.models import Observation
+from django.core.validators import MinValueValidator
+
 from django.utils.translation import gettext_lazy as _
 
 
 class Construction(models.Model):
-    project_name = models.CharField(max_length=250, null=False)
-    location = models.CharField(max_length=250, null=False)
-    description = models.TextField(null=False)
-    num_housing_units = models.IntegerField()
-    num_adapted_units = models.IntegerField()
-    land_area = models.DecimalField(max_digits=10, decimal_places=2)
+    project_name = models.CharField(
+        max_length=250, null=False, verbose_name=_("Project name")
+    )
 
-    # observations = models.ManyToManyField(
-    #     Observation, "Observacao"
-    # )  # Verificar melhor related name para um field externo.
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=250, null=False, verbose_name=_("Location"))
+    description = models.TextField(null=False, verbose_name=_("Description"))
+    
+    num_housing_units = models.IntegerField(
+        verbose_name=_("Number housing units"),
+        default=0,
+        validators=[MinValueValidator(0)],
+    )
+    
+    num_adapted_units = models.IntegerField(
+        verbose_name=_("Number adapted units"),
+        default=0,
+        validators=[MinValueValidator(0)],
+    )
+
+    land_area = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name=_("Land area")
+    )
+
+    observations = models.ManyToManyField(
+        Observation,
+        verbose_name=_("Observacao"),
+        through="SetConstructionObservations",
+        related_name="related_constructions",
+    )  # Verificar melhor related name para um field externo.
+
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True, verbose_name=_("Is active"))
 
     def __str__(self):
         return self.project_name
+
+
+class SetConstructionObservations(models.Model):
+    construction = models.ForeignKey(Construction, on_delete=models.CASCADE)
+    observation = models.ForeignKey(Observation, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "set_construction_observations"
+
+    def __str__(self):
+        return f"{self.construction.project_name} - {self.observation.description[:30]}"
--- apps/observations/models.py
+++ apps/observations/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Observação
 class Observation(models.Model):
-    description = models.TextField(verbose_name="description", blank=False)
-    # construction = models.ForeignKey(Obra, on_delete=models.DO_NOTHING, related_name="observations")
+    description = models.TextField(verbose_name=_("description"), blank=False)
+
     # created_at = models.DateTimeField(auto_now_add=True)
     # updated_at = models.DateTimeField(auto_now=True)
+
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
