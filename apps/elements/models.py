43119bbc8f037f1d21098f6b87338f08d34223ef
Kaio Vinícios <kvsgpro@outlook.com>
2025-09-08 15:10:27 -0300

    feat(elements): create ElementType and Element

--- apps/elements/models.py
+++ apps/elements/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from apps.materials.models import Material
 
 
-# Create your models here.
+class ElementType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+# SetElementTypeMaterials
 class Element(models.Model):
-    pass
+    element_type = models.ForeignKey(
+        ElementType, on_delete=models.CASCADE, related_name="elements"
+    )
+    materials = models.ManyToManyField(Material, related_name="elements")
+
+    # Talvez adicionar Meta -> UniqueConstraint para evitar recorrências iguais de element_type e materials
+
+    def __str__(self):
+        return f"Element: {self.element_type.name} - {self.materials}"
