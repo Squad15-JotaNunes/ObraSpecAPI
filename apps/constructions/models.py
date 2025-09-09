99a2db32a50dc5b71d8c8d19ce784c6adc1a738d
Kaio Vinícios <kvsgpro@outlook.com>
2025-09-08 22:31:17 -0300

    refactor(constructions): correct properties and add referentials

--- apps/constructions/models.py
+++ apps/constructions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from apps.observations.models import Observation
 from django.core.validators import MinValueValidator
+from apps.referentials.models import Referential
 
 from django.utils.translation import gettext_lazy as _
 
@@ -11,14 +12,16 @@ class Construction(models.Model):
     )
 
     location = models.CharField(max_length=250, null=False, verbose_name=_("Location"))
-    description = models.TextField(null=False, verbose_name=_("Description"))
-    
+    description = models.TextField(
+        max_length=1000, null=False, verbose_name=_("Description")
+    )
+
     num_housing_units = models.IntegerField(
         verbose_name=_("Number housing units"),
         default=0,
         validators=[MinValueValidator(0)],
     )
-    
+
     num_adapted_units = models.IntegerField(
         verbose_name=_("Number adapted units"),
         default=0,
@@ -26,31 +29,21 @@ class Construction(models.Model):
     )
 
     land_area = models.DecimalField(
-        max_digits=10, decimal_places=2, verbose_name=_("Land area")
+        max_digits=10,
+        decimal_places=2,
+        verbose_name=_("Land area"),
     )
 
+    referentials = models.ManyToManyField(
+        Referential, verbose_name=_(""), related_name="constructions"
+    )
     observations = models.ManyToManyField(
         Observation,
-        verbose_name=_("Observacao"),
-        through="SetConstructionObservations",
-        related_name="related_constructions",
-    )  # Verificar melhor related name para um field externo.
-
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+        verbose_name=_("Observations"),
+        related_name="constructions",
+    )
 
     is_active = models.BooleanField(default=True, verbose_name=_("Is active"))
 
     def __str__(self):
         return self.project_name
-
-
-class SetConstructionObservations(models.Model):
-    construction = models.ForeignKey(Construction, on_delete=models.CASCADE)
-    observation = models.ForeignKey(Observation, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "set_construction_observations"
-
-    def __str__(self):
-        return f"{self.construction.project_name} - {self.observation.description[:30]}"
