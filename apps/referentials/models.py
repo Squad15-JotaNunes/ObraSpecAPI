e4630f6402807ea793c7e99dcc60ff78a992fce2
Kaio Vinícios <kvsgpro@outlook.com>
2025-09-08 22:29:37 -0300

    feat(referentials): create ReferentialName and Referential

--- apps/referentials/models.py
+++ apps/referentials/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+from apps.areas.models import Area
 
 
-# Create your models here.
+class ReferentialName(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Referential(models.Model):
-    name = models.CharField(max_length=50)
+    referential_name = models.ForeignKey(
+        ReferentialName, on_delete=models.CASCADE, related_name="referentials"
+    )
+    areas = models.ManyToManyField(Area, related_name="referentials")
+
     is_approved = models.BooleanField(default=False)
-    comment = models.TextField(max_length=500)
-    # Falta ambientes
+    comment = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return f"Referential: {self.referential_name.name} - {self.areas}"
