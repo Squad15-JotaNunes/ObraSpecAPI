280b978c95586c60d8a64ccfc0e510e69d139c31
Kaio Vinícios <kvsgpro@outlook.com>
2025-09-08 22:28:44 -0300

    feat(areas): create Area and AreaName

--- apps/areas/models.py
+++ apps/areas/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from apps.elements.models import Element
 
 
-# Ambiente
+# Nome Ambiente
+class AreaName(models.Model):
+    name = models.CharField(_("Name"), max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+# SetAreaElements
 class Area(models.Model):
-    name = models.CharField(_("Name"), max_length=50)
-    # adicionar conjunto de elementos.
+    area_name = models.ForeignKey(
+        AreaName, on_delete=models.CASCADE, related_name="areas"
+    )
+    elements = models.ManyToManyField(Element, related_name="areas")
 
-
-class SetAreaElements(models.Model):
-    pass
+    def __str__(self):
+        return f"Area: {self.area_name.name} - {self.elements}"
