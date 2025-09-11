ed7b724a63dfa3e8c08193179d9d9bd56fd1f611
Kaio Vinícios <kvsgpro@outlook.com>
2025-09-11 20:03:46 -0300

    feat(standard_models): define initial properties and create_construction

--- apps/standard_models/models.py
+++ apps/standard_models/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 from apps.observations.models import Observation
+from apps.constructions.models import Construction
+from apps.referentials.models import Referential
+from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
 class StandardModel(models.Model):
-    description = models.TextField(null=False)
-    num_housing_units = models.IntegerField()
-    num_adapted_units = models.IntegerField()
+    name = models.CharField(max_length=250, verbose_name=_("Standard name"))
+
+    num_housing_units = models.IntegerField(default=0)
+    num_adapted_units = models.IntegerField(default=0)
     land_area = models.DecimalField(max_digits=10, decimal_places=2)
 
-    observations = models.ManyToManyField(
-        Observation, "Observacao"
-    )  # Verificar melhor related name para um field externo.
+    referentials = models.ManyToManyField(Referential, related_name="standard_models")
+    observations = models.ManyToManyField(Observation, related_name="standard_models")
+
+    def __str__(self):
+        return self.name
+
+    def create_construction(self, project_name, location, description):
+        construction = Construction.objects.create(
+            project_name=project_name,
+            location=location,
+            description=description,
+            num_housing_units=self.num_housing_units,
+            num_adapted_units=self.num_adapted_units,
+            land_area=self.land_area,
+        )
+        construction.referentials.set(self.referentials.all())
+        construction.observations.set(self.observations.all())
+        return construction
