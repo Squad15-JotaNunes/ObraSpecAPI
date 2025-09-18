6b9cd23223d3dcf2aa923894458c2427f460a5bd
Joao Victor <jvmlyra@gmail.com>
2025-09-18 20:02:58 -0300

    feat -> Elements views and urls

--- apps/elements/services/list_all.py
+++
@@ -1,8 +0,0 @@
-from apps.elements.models import Element
-from rest_framework.response import Response
-
-class ListElementService:
-    @staticmethod
-    def service():
-        elements = Element.objects.all().values()
-        return Response(list(elements))
--- apps/elements/views.py
+++ apps/elements/views.py
@@ -1,26 +1,22 @@
 from rest_framework.views import APIView
 
-from .services.create import CreateElementService
-from .services.list_all import ListElementService
-from .services.get_one import RetrieveElementService
-from .services.update import UpdateElementService
-from .services.delete import DeleteElementService
+from .services.services import ElementsServices
 
 
 class ElementsListAPIView(APIView):
     def get(self, request):
-        return ListElementService.service()
+        return ElementsServices.list()
 
     def post(self, request):
-        return CreateElementService.service(request)
+        return ElementsServices.create(request)
 
 
 class ElementsDetailAPIView(APIView):
     def get(self, request, pk):
-        return RetrieveElementService.service(pk)
+        return ElementsServices.get(pk)
 
     def patch(self, request, pk):
-        return UpdateElementService.service(pk, request.data)
+        return ElementsServices.update(pk, request.data)
 
     def delete(self, request, pk):
-        return DeleteElementService.service(pk)
+        return ElementsServices.delete(pk)
