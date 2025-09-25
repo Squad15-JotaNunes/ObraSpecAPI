7c3a4baddfe2b306ad76a4a1f723a4217c1be894
Joao Victor <jvmlyra@gmail.com>
2025-09-25 15:11:10 -0300

    feat: implement elements views, serializers, and services

--- apps/elements/views.py
+++ apps/elements/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import NotFound
 
-# Create your views here.
+from .services import ElementTypeServices, ElementsServices
+from apps.elements.serializers import (
+    ElementTypeSerializer,
+    ElementSerializer,
+)
+
+
+class ElementTypeListAPIView(APIView):
+    def get(self, request):
+        element_types = ElementTypeServices.list_all()
+        serializer = ElementTypeSerializer(element_types, many=True)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ElementTypeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
+
+
+class ElementTypeDetailAPIView(APIView):
+    def get(self, request, pk):
+        element_type = ElementTypeServices.get(pk)
+        serializer = ElementTypeSerializer(element_type)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        element_type = ElementTypeServices.get(pk)
+        serializer = ElementTypeSerializer(element_type, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        ElementTypeServices.delete(pk)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ElementsListAPIView(APIView):
+    def get(self, request):
+        elements = ElementsServices.list_all()
+        serializer = ElementSerializer(elements, many=True)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ElementSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
+
+
+class ElementsDetailAPIView(APIView):
+    def get(self, request, pk):
+        element = ElementsServices.get(pk)
+        serializer = ElementSerializer(element)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        element = ElementsServices.get(pk)
+        serializer = ElementSerializer(element, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        ElementsServices.delete(pk)
+        return Response(status=status.HTTP_204_NO_CONTENT)
