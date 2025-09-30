568a16a67ffa4e90c3b64be37a27adefb6815d4d
Kaio Vinícios <kvsgpro@outlook.com>
2025-09-30 19:50:13 -0300

    fix(materials): add many=True in some POST methods

--- apps/materials/views.py
+++ apps/materials/views.py
@@ -7,33 +7,30 @@ from .services import BrandServices, MaterialsServices, MaterialTypeServices
 from apps.materials.serializers import (
     BrandSerializer,
     MaterialTypeSerializer,
-    MaterialsSerializer,
+    MaterialSerializer,
 )
 
 
 class BrandListAPIView(APIView):
-    # Testado e ok
     def get(self, request):
         brands = BrandServices.list_all()
         serializer = BrandSerializer(brands, many=True)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
+    # Recebe um ou mais objetos, contanto que presentes contidos em []
     def post(self, request):
-        serializer = BrandSerializer(data=request.data)
+        serializer = BrandSerializer(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
-        serializer.save()  # cria o objeto
+        serializer.save()
         return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
 
 
 class BrandDetailAPIView(APIView):
-    # Testado e ok
     def get(self, request, pk):
         brand = BrandServices.get(pk)
         serializer = BrandSerializer(brand)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
     def patch(self, request, pk):
         brand = BrandServices.get(pk)
         serializer = BrandSerializer(brand, data=request.data, partial=True)
@@ -41,35 +38,30 @@ class BrandDetailAPIView(APIView):
         serializer.save()
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
     def delete(self, request, pk):
         BrandServices.delete(pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class MaterialTypeListAPIView(APIView):
-    # Testado e ok
     def get(self, request):
         types_materials = MaterialTypeServices.list_all()
         serializer = MaterialTypeSerializer(types_materials, many=True)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
     def post(self, request):
-        serializer = MaterialTypeSerializer(data=request.data)
+        serializer = MaterialTypeSerializer(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
 
 
 class MaterialTypeDetailApiView(APIView):
-    # Testado e ok
     def get(self, request, pk):
         material_type = MaterialTypeServices.get(pk)
         serializer = MaterialTypeSerializer(material_type)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
     def patch(self, request, pk):
         material_type = MaterialTypeServices.get(pk)
         serializer = MaterialTypeSerializer(
@@ -79,43 +71,37 @@ class MaterialTypeDetailApiView(APIView):
         serializer.save()
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
     def delete(self, request, pk):
         MaterialTypeServices.delete(pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class MaterialsListAPIView(APIView):
-    # Testado e ok
     def get(self, request):
         materials = MaterialsServices.list_all()
-        serializer = MaterialsSerializer(materials, many=True)
+        serializer = MaterialSerializer(materials, many=True)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
     def post(self, request):
-        serializer = MaterialsSerializer(data=request.data)
+        serializer = MaterialSerializer(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
 
 
 class MaterialsDetailApiView(APIView):
-    # Testado e ok
     def get(self, request, pk):
         material = MaterialsServices.get(pk)
-        serializer = MaterialsSerializer(material)
+        serializer = MaterialSerializer(material)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
     def patch(self, request, pk):
         material = MaterialsServices.get(pk)
-        serializer = MaterialsSerializer(material, data=request.data, partial=True)
+        serializer = MaterialSerializer(material, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
-    # Testado e ok
     def delete(self, request, pk):
         MaterialsServices.delete(pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
