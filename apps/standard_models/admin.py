9eb2af59b868fbf0721492bc7d86b1b7c65bec03
Kaio Vinícios <kvsgpro@outlook.com>
2025-10-21 23:10:30 -0300

    feat(standard_models): add StandardModelAdmin

--- apps/standard_models/admin.py
+++ apps/standard_models/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.standard_models.models import StandardModel
 
-# Register your models here.
+@admin.register(StandardModel)
+class StandardModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
