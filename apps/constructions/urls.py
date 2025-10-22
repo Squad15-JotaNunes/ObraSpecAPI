5cf6c06142775c54679bb187b9a68583906a5ea3
Kaio Vinícios <kvsgpro@outlook.com>
2025-10-22 18:38:35 -0300

    feat(constructions): create using-standard-model path

--- apps/constructions/urls.py
+++ apps/constructions/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import ConstructionsListAPIView, ConstructionsDetailAPIView
+from .views import (
+    ConstructionsListAPIView,
+    ConstructionsDetailAPIView,
+    ConstructionBasedStandardModelsAPIView,
+)
 
-app_name = "constructions"
 
 urlpatterns = [
     path("", ConstructionsListAPIView.as_view(), name="constructions_list"),
-    path("<int:pk>/", ConstructionsDetailAPIView.as_view(), name="constructions_detail"),
+    path(
+        "<int:pk>/", ConstructionsDetailAPIView.as_view(), name="constructions_detail"
+    ),
+    path(
+        "using-standard-model/<int:pk>/",
+        ConstructionBasedStandardModelsAPIView.as_view(),
+        name="construction_based_standard_models",
+    ),
 ]
