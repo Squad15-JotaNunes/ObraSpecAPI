4c0b3260e779df5b47fd3eef75d8bfa315f49d17
Kaio Vinícios <kvsgpro@outlook.com>
2025-10-22 18:39:45 -0300

    refactor(setup): add api_patterns constant

--- setup/urls.py
+++ setup/urls.py
@@ -1,25 +1,20 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+api_patterns = [
+    path("observations/", include("apps.observations.urls")),
+    path("materials/", include("apps.materials.urls")),
+    path("elements/", include("apps.elements.urls")),
+    path("areas/", include("apps.areas.urls")),
+    path("referentials/", include("apps.referentials.urls")),
+    path("standard-models/", include("apps.standard_models.urls")),
+    path("accounts/", include("apps.accounts.urls")),
+    path("authentication/", include("apps.authentication.urls")),
+    path("constructions/", include("apps.constructions.urls")),
+]
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -31,17 +26,12 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=[AllowAny],
+    patterns=api_patterns,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("observations/", include("apps.observations.urls")),
-    path("materials/", include("apps.materials.urls")),
-    path("elements/", include("apps.elements.urls")),
-    path("areas/", include("apps.areas.urls")),
-    path("referentials/", include("apps.referentials.urls")),
-    path("standard-models/", include("apps.standard_models.urls")),
-    path("accounts/", include("apps.accounts.urls")),
+    *api_patterns,
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
@@ -53,5 +43,4 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("authentication/", include("apps.authentication.urls")),
 ]
